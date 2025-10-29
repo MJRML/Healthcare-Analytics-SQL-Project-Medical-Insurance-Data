c7319c6255472e577639f5dee76286efd5227608
MJR <martynryan947@gmail.com>
2025-10-29 11:10:03 +0400

    Add query for average charges by smoking status
    Add a query to calculate average medical charges by smoking status.

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -1,1 +1,8 @@
+-- Query 1: Average Charges by Smoking Status
+-- Description: Compare average medical costs between smokers and non-smokers
 
+SELECT smoker, 
+       ROUND(AVG(charges), 2) AS avg_charges
+FROM medical_insurance
+GROUP BY smoker
+ORDER BY avg_charges DESC;
